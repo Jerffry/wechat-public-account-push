2bb20095a8e5721c74dbe5b755a3b53d88fb5ab5
Jerffry <57864661+Jerffry@users.noreply.github.com>
2023-06-15 00:23:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oXLyd6erdO-Gz9-HzS4abEE393Ho',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '00yqv_0EVa_QfaV860Q2puOhUoQ0B3SvjSv9YZHZb54',
+      useTemplateId: 'dihB2HFRrqZKtRAp3vGNbuMZVH1_sRslH8JEu0It3zE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-08',
       festivals: [
